fc56575c1350b850e3760ce44175e5dc5d321820
Pikachoo <novakovsk@gmail.com>
2015-12-07 22:51:18 +0300

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,10 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.paths << Rails.root.join("app", "assets", "fonts","assets/themes/default/assetes/fonts", "themes/default/assets/fonts")
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
